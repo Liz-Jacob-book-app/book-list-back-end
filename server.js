865897003a81a00cdb015babe13989072f64d6ff
pereztjacob <pereztjacob@gmail.com>
2017-12-07 19:48:34 -0800

    no significant changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// module?? 
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
 // const fs = require('fs');
 const pg = require('pg');
+const fs = require('fs');
 const cors = require('cors');
 const PORT = process.env.PORT;
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -19,7 +22,7 @@ app.get('/api/v1/books', (req, res) => {
     // n.b. data is returned as json data in console
 });
 
-// app.get(`/api/v1/books`, (req, res) => {
+// app.get(`/api/v1/books:inst`, (req, res) => {
 //     client.query(`SELECT * FROM books WHERE book = $1`, [req.params.books])
 //         .then(data => res.send(data.rows));
 // });
@@ -30,31 +33,31 @@ app.listen(PORT, () => {
 
 ///// DATABASE HANDLER //////////
 
-//function loadBooks() {
-//    fs.readFile('./data/books.json', (err, fd) => {
-//        JSON.parse(fd.toString()).forEach(ele => {
-//            client.query(
-//                'INSERT INTO books "(book_id, title, author, isbn, "imageUrl", description) VALUES" ($1, $2, $3, $4, //$5, $6) ON CONFLICT DO NOTHING',
-//                [ele.book_id, ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
-//            )
-//                .catch(console.error);
-//        });
-//    });
-//}
+function loadBooks() {
+    fs.readFile('./data/books.json', (err, fd) => {
+        JSON.parse(fd.toString()).forEach(ele => {
+            client.query(
+                'INSERT INTO books "(book_id, title, author, isbn, "imageUrl", description) VALUES" ($1, $2, $3, $4, //$5, $6) ON CONFLICT DO NOTHING',
+                [ele.book_id, ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
+            )
+                .catch(console.error);
+        });
+    });
+}
 
-//function loadDB() {
-//    client.query(`
-//    CREATE TABLE IF NOT EXISTS
-//    books (
-//      book_id SERIAL PRIMARY KEY,
-//      title VARCHAR(50),
-//      author VARCHAR(50),
-//      isbn VARCHAR(50),
-//      "imageUrl" VARCHAR(255),
-//      description VARCHAR(255));`
-//    )
-//        .then(loadBooks)
-//        .catch(console.error);
-//}
+function loadDB() {
+    client.query(`
+    CREATE TABLE IF NOT EXISTS
+    books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    author VARCHAR(50),
+    isbn VARCHAR(50),
+    "imageUrl" VARCHAR(255),
+    description VARCHAR(2000));`
+    )
+        .then(loadBooks)
+        .catch(console.error);
+}
 
-//loadDB();
+loadDB();
